e67dbe9af40570123255c443dae6f1e06122a94c
Anne Quinkenstein <a.quinkenstein@gmail.com>
2021-02-21 18:40:48 +0100

    delete mp4

--- src/App.js
+++ src/App.js
@@ -4,23 +4,22 @@ import "./App.css";
 const App = () => {
   return (
     <div className="App">
-      <img src="/img/00_grab.jpg" alt="image" />
-      <img src="/img/kartons02.jpg" alt="image" />
-      <img src="/img/kartons022.jpg" alt="image" />
-      <img src="/img/kartons023.jpg" alt="image" />
-      <img src="/img/kartons024.jpg" alt="image" />
-      <img src="/img/kartons025.jpg" alt="image" />
-      <img src="/img/kartons026.jpg" alt="image" />
-      <img src="/img/kartons027.jpg" alt="image" />
-      <img src="/img/kartons021.mp4" alt="image" />
+      <img src="/img/00_grab.jpg" alt="Grab" />
+      <img src="/img/kartons02.jpg" alt="Kartonsanordnung 2" />
+      <img src="/img/kartons022.jpg" alt="Kartonsanordnung 2" />
+      <img src="/img/kartons023.jpg" alt="Kartonsanordnung 2" />
+      <img src="/img/kartons024.jpg" alt="Kartonsanordnung 2" />
+      <img src="/img/kartons025.jpg" alt="Kartonsanordnung 2" />
+      <img src="/img/kartons026.jpg" alt="Kartonsanordnung 2" />
+      <img src="/img/kartons027.jpg" alt="Kartonsanordnung 2" />
+      <img src="/img/kartons021.mp4" alt="Kartonsanordnung 2" />
       {/* <video controls autostart autoPlay src={Video} type="video/mp4" /> */}
-      <img src="/img/kartons011.jpg" alt="image" />
-      <img src="/img/kartons012.jpg" alt="image" />
-      <img src="/img/kartons013.jpg" alt="image" />
-      <img src="/img/kartons014.jpg" alt="image" />
-      
+      <img src="/img/kartons011.jpg" alt="Kartonsanordnung 1" />
+      <img src="/img/kartons012.jpg" alt="Kartonsanordnung 1" />
+      <img src="/img/kartons013.jpg" alt="Kartonsanordnung 1" />
+      <img src="/img/kartons014.jpg" alt="Kartonsanordnung 1" />
     </div>
   );
-}
+};
 
 export default App;
