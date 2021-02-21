1a63c78991a13d39dee9d03985eed32e9d187096
Anne Quinkenstein <a.quinkenstein@gmail.com>
2021-02-21 18:51:48 +0100

    config mesures of pics

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import "./App.css";
 const App = () => {
   return (
     <div className="App">
-      <img src="/img/00_grab.jpg" alt="Grab" />
       <img src="/img/kartons02.jpg" alt="Kartonsanordnung 2" />
       <img src="/img/kartons022.jpg" alt="Kartonsanordnung 2" />
       <img src="/img/kartons023.jpg" alt="Kartonsanordnung 2" />
@@ -12,12 +11,11 @@ const App = () => {
       <img src="/img/kartons025.jpg" alt="Kartonsanordnung 2" />
       <img src="/img/kartons026.jpg" alt="Kartonsanordnung 2" />
       <img src="/img/kartons027.jpg" alt="Kartonsanordnung 2" />
-      <img src="/img/kartons021.mp4" alt="Kartonsanordnung 2" />
       {/* <video controls autostart autoPlay src={Video} type="video/mp4" /> */}
       <img src="/img/kartons011.jpg" alt="Kartonsanordnung 1" />
       <img src="/img/kartons012.jpg" alt="Kartonsanordnung 1" />
       <img src="/img/kartons013.jpg" alt="Kartonsanordnung 1" />
-      <img src="/img/kartons014.jpg" alt="Kartonsanordnung 1" />
+      <img src="/img/00_grab.jpg" alt="Grab" />
     </div>
   );
 };
