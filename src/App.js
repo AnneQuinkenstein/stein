0c62cd787b858eebde3f9dea73b4642620f1cf1e
Anne Quinkenstein <a.quinkenstein@gmail.com>
2021-02-21 19:20:18 +0100

    config mesures of pics

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
 import "./App.css";
-// import Video from "./image/kartons021.mp4";
 
 const App = () => {
   return (
-    <div className="App">
-      <img src="/img/kartons02.jpg" alt="Kartonsanordnung 2" />
-      <img src="/img/kartons022.jpg" alt="Kartonsanordnung 2" />
-      <img src="/img/kartons023.jpg" alt="Kartonsanordnung 2" />
-      <img src="/img/kartons024.jpg" alt="Kartonsanordnung 2" />
-      <img src="/img/kartons025.jpg" alt="Kartonsanordnung 2" />
-      <img src="/img/kartons026.jpg" alt="Kartonsanordnung 2" />
-      <img src="/img/kartons027.jpg" alt="Kartonsanordnung 2" />
-      {/* <video controls autostart autoPlay src={Video} type="video/mp4" /> */}
-      <img src="/img/kartons011.jpg" alt="Kartonsanordnung 1" />
-      <img src="/img/kartons012.jpg" alt="Kartonsanordnung 1" />
-      <img src="/img/kartons013.jpg" alt="Kartonsanordnung 1" />
-      <img src="/img/00_grab.jpg" alt="Grab" />
-    </div>
+    <>
+      <h1>Kartons eher seitlich </h1>
+      <div className="App">
+        <img src="/img/kartons02.jpg" alt="Kartonsanordnung 2" />
+        <img src="/img/kartons022.jpg" alt="Kartonsanordnung 2" />
+        <img src="/img/kartons023.jpg" alt="Kartonsanordnung 2" />
+        <img src="/img/kartons027.jpg" alt="Kartonsanordnung 2" />
+        <img src="/img/kartons024.jpg" alt="Kartonsanordnung 2" />
+        <img src="/img/kartons025.jpg" alt="Kartonsanordnung 2" />
+        <img src="/img/kartons026.jpg" alt="Kartonsanordnung 2" />
+        {/* <video controls autostart autoPlay src="https://app.box.com/s/rv1ek3chvh47w8gwn1jfc5c3869yszxe" type="video/mp4" /> */}
+      </div>
+      <h1>Kartons eher hinten </h1>
+      <div className="App">       
+        <img src="/img/kartons011.jpg" alt="Kartonsanordnung 1" />
+        <img src="/img/kartons012.jpg" alt="Kartonsanordnung 1" />
+        <img src="/img/kartons013.jpg" alt="Kartonsanordnung 1" />
+        <img src="/img/00_grab.jpg" alt="Grab" />
+        
+      </div>
+    </>
   );
 };
 
