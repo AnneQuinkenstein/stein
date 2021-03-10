2f4892e5bcf1d75ad42c97bf9e157e7191cfe70d
Anne Quinkenstein <a.quinkenstein@gmail.com>
2021-03-10 19:55:58 +0100

    more pics

--- src/App.js
+++ src/App.js
@@ -27,18 +27,6 @@ const App = () => {
         <img src="/img/kartons026.jpg" alt="Kartonsanordnung 2" />
         {/* <video controls autostart autoPlay src="https://app.box.com/s/rv1ek3chvh47w8gwn1jfc5c3869yszxe" type="video/mp4" /> */}
       </div>
-
-      <h1>Kartons eher seitlich </h1>
-      <div className="App">
-        <img src="/img/kartons02.jpg" alt="Kartonsanordnung 2" />
-        <img src="/img/kartons022.jpg" alt="Kartonsanordnung 2" />
-        <img src="/img/kartons023.jpg" alt="Kartonsanordnung 2" />
-        <img src="/img/kartons027.jpg" alt="Kartonsanordnung 2" />
-        <img src="/img/kartons024.jpg" alt="Kartonsanordnung 2" />
-        <img src="/img/kartons025.jpg" alt="Kartonsanordnung 2" />
-        <img src="/img/kartons026.jpg" alt="Kartonsanordnung 2" />
-        {/* <video controls autostart autoPlay src="https://app.box.com/s/rv1ek3chvh47w8gwn1jfc5c3869yszxe" type="video/mp4" /> */}
-      </div>
       <h1>Kartons eher hinten </h1>
       <div className="App">
         <img src="/img/kartons011.jpg" alt="Kartonsanordnung 1" />
@@ -51,6 +39,9 @@ const App = () => {
         <img src="/img/Sommer5.jpg" alt="Grab im Sommer" />
         <img src="/img/Sommer6.jpg" alt="Grab im Sommer" />
         <img src="/img/Sommer7.jpg" alt="Grab im Sommer" />
+        <img src="/img/Grabstein-entwurf-2.jpg" alt="Grab im Sommer" />
+        <img src="/img/Grabstein-entwurf-2b.jpg" alt="Grab im Sommer" />
+        <img src="/img/steinanordnung.jpg" alt="Steine" />
       </div>
     </>
   );
