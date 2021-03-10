8616e53ec6eed995059820fb985e83caacfe652a
Anne Quinkenstein <a.quinkenstein@gmail.com>
2021-03-10 19:35:26 +0100

    stein links

--- src/App.js
+++ src/App.js
@@ -3,6 +3,15 @@ import "./App.css";
 const App = () => {
   return (
     <>
+      <h1>Steine </h1>
+      <a href="https://www.obernkirchener-sandstein.de/de/farben"> 
+        Obernkirchener Sandstein
+      </a>
+      <br/>
+      <a href="https://www.obernkirchener-sandstein.de/de/bearbeitete-oberflaechen">
+        Oberflächenbearbeitungen
+      </a>
+
       <h1>Kartons eher seitlich </h1>
       <div className="App">
         <img src="/img/kartons02.jpg" alt="Kartonsanordnung 2" />
@@ -15,17 +24,17 @@ const App = () => {
         {/* <video controls autostart autoPlay src="https://app.box.com/s/rv1ek3chvh47w8gwn1jfc5c3869yszxe" type="video/mp4" /> */}
       </div>
       <h1>Kartons eher hinten </h1>
-      <div className="App">       
+      <div className="App">
         <img src="/img/kartons011.jpg" alt="Kartonsanordnung 1" />
         <img src="/img/kartons012.jpg" alt="Kartonsanordnung 1" />
         <img src="/img/kartons013.jpg" alt="Kartonsanordnung 1" />
       </div>
       <h1> weitere </h1>
-      <div className="App">       
+      <div className="App">
         <img src="/img/00_grab.jpg" alt="Grab" />
         <img src="/img/Sommer5.jpg" alt="Grab im Sommer" />
         <img src="/img/Sommer6.jpg" alt="Grab im Sommer" />
-        <img src="/img/Sommer7.jpg" alt="Grab im Sommer" />        
+        <img src="/img/Sommer7.jpg" alt="Grab im Sommer" />
       </div>
     </>
   );
